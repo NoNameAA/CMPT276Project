50884c053eb8a47de185d3c4ee09af4e80db81b0
NoNameAA <sitel@sfu.ca>
2017-06-23 02:07:00 +0000

    home page controller created  ||  about page controller created  ||  home page view created

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
-    root 'application#hello'
+  get 'about_page/about'
+  get 'home_page/home'
+
+  resources :users
+    root 'home_page#home'
+    #root_url 'https://mysterious-beyond-23272.herokuapp.com'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
