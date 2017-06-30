07784411927824f142685196395c212f297b99e7
NoNameAA <sitel@sfu.ca>
2017-06-30 23:22:26 +0000

    change the root page to users/new

--- config/routes.rb
+++ config/routes.rb
@@ -20,7 +20,7 @@ Rails.application.routes.draw do
 
 
   resources :users
-    root 'login#login_page'
+    root 'users#new'
     #root_url 'https://mysterious-beyond-23272.herokuapp.com'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
